aa654ee96a3524726437c13e6d965957f30ca5c6
(Estudiante) Andrei David Flores Espinoza <andrei.flores@udla.edu.ec>
2025-05-16 09:33:39 -0500

    Cambios

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from routes.main import main_bp
-from routes.universidades import universidades_bp
 
 app = Flask(__name__)
 app.register_blueprint(main_bp)
-app.register_blueprint(universidades_bp)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
